d78ffc12903b35633d3decfb4c1879946adc24ac
Stuart Lewis <stuart@stuartlewis.com>
2011-04-21 20:19:22 +1200

    Updating code and README for initial v2 development work

--- test/test-swordappclient.php
+++ test/test-swordappclient.php
@@ -1,33 +1,38 @@
 <?php
-	
+    // Test the V2 PHP client implementation using the Simple SWORD Server (SSS)
+
 	// The URL of the service document
-	$testurl = "http://client.swordapp.org/client/servicedocument";
+	$testurl = "http://localhost:8080/sd-uri";
 	
 	// The user (if required)
-	$testuser = "sword-user";
+	$testuser = "sword";
 	
 	// The password of the user (if required)
-	$testpw = "sword-user";
+	$testpw = "sword";
 	
 	// The on-behalf-of user (if required)
 	//$testobo = "user@swordapp.com";
 
 	// The URL of the example deposit collection
-	$testdepositurl = "http://client.swordapp.org/client/deposit/sword-user";
+	$testdepositurl = "http://localhost:8080/col-uri/176da419-7af6-431b-b1a8-b00a1898bcae";
+
+	// The test atom entry to deposit
+	$testatom = "test-files/atom_multipart/atom";
 
 	// The test file to deposit
-	$testfile = "test-files/sword-article.zip";
+	$testfile = "test-files/atom_multipart_package.zip";
+	//$testfile = "/Users/stuartlewis/sss/example.zip";
 
 	// The content type of the test file
 	$testcontenttype = "application/zip";
 
-	// The packaing format of the test fifle
-	$testformat = "http://purl.org/net/sword-types/METSDSpaceSIP";
+	// The packaging format of the test fifle
+	$testformat = "http://purl.org/net/sword/package/default";
 	
 	require("../swordappclient.php");
 	$testsac = new SWORDAPPClient();
 
-	if (true) {
+	if (false) {
 		print "About to request servicedocument from " . $testurl . "\n";
 		if (empty($testuser)) { print "As: anonymous\n"; }
 		else { print "As: " . $testuser . "\n"; }
@@ -87,12 +92,11 @@
 		print "About to deposit file (" . $testfile . ") to " . $testdepositurl . "\n";
 		if (empty($testuser)) { print "As: anonymous\n"; }
 		else { print "As: " . $testuser . "\n"; }
-		$testdr = $testsac->deposit(
-		              $testdepositurl, $testuser, $testpw, $testobo, $testfile, $testformat, $testcontenttype);
+		$testdr = $testsac->depositMultipart($testdepositurl, $testuser, $testpw, $testobo, $testatom, $testfile, $testformat, $testcontenttype);
 		print "Received HTTP status code: " . $testdr->sac_status . 
 		      " (" . $testdr->sac_statusmessage . ")\n";
 		
-		if ($testdr->sac_status == 201) {
+		if (($testdr->sac_status >= 200) || ($testdr->sac_status < 300)) {
 			print " - ID: " . $testdr->sac_id . "\n";
 			print " - Title: " . $testdr->sac_title . "\n";
 			print " - Content: " . $testdr->sac_content_src . 
@@ -116,52 +120,7 @@
 			if (!empty($testdr->sac_noOp)) { print " - noOp: " . $testdr->sac_noOp . "\n"; }
 		}
 	}
-	
+
 	print "\n\n";
-	
-	if (false) {
-		$testdepositURL = "http://client.swordapp.org/client/deposit/s-2";
-		$testobo = "fail";
-		print "About to deposit file (" . $testfile . ") to " . $testdepositurl . "\n";
-		print "This deposit should fail!\n";
-		if (empty($testuser)) { print "As: anonymous\n"; }
-		else { print "As: " . $testuser . "\n"; }
-		$testdr = $testsac->deposit(
-		              $testdepositurl, $testuser, $testpw, $testobo, $testfile, "METS/SWAP");
-		print "Received HTTP status code: " . $testdr->sac_status . 
-		      " (" . $testdr->sac_statusmessage . ")\n";
-		
-		if ($testdr->sac_status == 201) {
-			print " - ID: " . $testdr->sac_id . "\n";
-			print " - Title: " . $testdr->sac_title . "\n";
-			print " - Content: " . $testdr->sac_content_src . 
-			      " (" . $testdr->sac_content_type . ")\n";
-			foreach ($testdr->sac_authors as $author) {
-				print "  - Author: " . $author . "\n";
-			}
-			foreach ($testdr->sac_contributors as $contributor) {
-				print "  - Contributor: " . $contributor . "\n";
-			}
-			foreach ($testdr->sac_links as $link) {
-				print "  - Link: " . $link . "\n";
-			}
-			print " - Summary: " . $testdr->sac_summary . "\n";
-			print " - Updated: " . $testdr->sac_updated . "\n";
-			print " - Rights: " . $testdr->sac_rights . "\n";
-			print " - Packaging: " . $testdr->sac_packaging . "\n";
-			print " - Generator: " . $testdr->sac_generator . 
-			      " (" . $testdr->sac_generator_uri . ")\n";
-			print " - User agent: " . $testdr->sac_useragent . "\n";
-			if (!empty($testdr->sac_noOp)) { print " - noOp: " . $testdr->sac_noOp . "\n"; }
-		} else {
-			// $testdr should be an instance of SWORDAPPErrorDocument
-			print " - Error URI: " . $testdr->sac_erroruri . "\n";
-			if (!empty($testdr->sac_generator)) {
-				print "  - Generator: " . $testdr->sac_generator . 
-			              " (" . $testdr->sac_generator_uri . ")\n";
-			}
-			print "  - Title: " . $testdr->sac_title . "\n";
-			print "  - Summary: " . $testdr->sac_summary . "\n";
-		}
-	}
+
 ?>
