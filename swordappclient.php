9adebef21550e77692d5e03d8fe7e552f2ae8833
Stuart Lewis <stuart@stuartlewis.com>
2011-05-14 20:15:42 +1200

    Numerous updates, including removing X- from headers, adding content-length and MD5 to multipart file deposit part

--- swordappclient.php
+++ swordappclient.php
@@ -6,17 +6,18 @@ require("swordapperrordocument.php");
 require("swordapplibraryuseragent.php");
 
 class SWORDAPPClient {
-	
-	// Request a servicedocument at the specified url, with the specified credentials,
+
+    private $debug = false;
+
+	// Request a Service Document from the specified url, with the specified credentials,
 	// and on-behalf-of the specified user.
 	function servicedocument($sac_url, $sac_u, $sac_p, $sac_obo) {
 		// Get the service document
 		$sac_curl = curl_init();
-		
+
 		curl_setopt($sac_curl, CURLOPT_RETURNTRANSFER, true);
-		// To see debugging information, un-comment the following line
-		//curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
-		
+		if ($this->debug) curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
+
 		curl_setopt($sac_curl, CURLOPT_URL, $sac_url);
 		if(!empty($sac_u) && !empty($sac_p)) {
 	        curl_setopt($sac_curl, CURLOPT_USERPWD, $sac_u . ":" . $sac_p);
@@ -41,23 +42,22 @@ class SWORDAPPClient {
             }
 		} else {
 			$sac_sdresponse = new SWORDAPPServiceDocument($sac_url, $sac_status);
-		}	
+		}
 
-		// Return the servicedocument object
+		// Return the Service Document object
 		return $sac_sdresponse;
 	}
 
 	// Perform a deposit to the specified url, with the specified credentials,
 	// on-behalf-of the specified user, and with the given file and formatnamespace and noop setting
-	function deposit($sac_url, $sac_u, $sac_p, $sac_obo, $sac_fname, 
-	                 $sac_packaging= '', $sac_contenttype = '', 
+	function deposit($sac_url, $sac_u, $sac_p, $sac_obo, $sac_fname,
+	                 $sac_packaging= '', $sac_contenttype = '',
         			 $sac_noop = false, $sac_verbose = false) {
 		// Perform the deposit
 		$sac_curl = curl_init();
 
-		// To see debugging information, un-comment the following line
-		//curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
-		
+		if ($this->debug) curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
+
 		curl_setopt($sac_curl, CURLOPT_URL, $sac_url);
 		curl_setopt($sac_curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($sac_curl, CURLOPT_POST, true);
@@ -70,21 +70,15 @@ class SWORDAPPClient {
 		$sac_md5 = md5_file($sac_fname);
 		array_push($headers, "Content-MD5: " . $sac_md5);
 		if (!empty($sac_obo)) {
-			array_push($headers, "X-On-Behalf-Of: " . $sac_obo);
+			array_push($headers, "On-Behalf-Of: " . $sac_obo);
         }
 		if (!empty($sac_packaging)) {
-			array_push($headers, "X-Packaging: " . $sac_packaging);
+			array_push($headers, "Packaging: " . $sac_packaging);
         }
 		if (!empty($sac_contenttype)) {
 			array_push($headers, "Content-Type: " . $sac_contenttype);
         }
 		array_push($headers, "Content-Length: " . filesize($sac_fname));
-		if ($sac_noop == true) {
-			array_push($headers, "X-No-Op: true");
-        }
-		if ($sac_verbose == true) {
-			array_push($headers, "X-Verbose: true");
-        }
 		$index = strpos(strrev($sac_fname), '/');
         if ($index === false) {
             $index = strlen($sac_fname) - $index;
@@ -95,7 +89,7 @@ class SWORDAPPClient {
 		array_push($headers, "Content-Disposition: filename=" . $sac_fname_trimmed);
 		curl_setopt($sac_curl, CURLOPT_READDATA, fopen($sac_fname, 'rb'));
         curl_setopt($sac_curl, CURLOPT_HTTPHEADER, $headers);
-		
+
 		$sac_resp = curl_exec($sac_curl);
 		$sac_status = curl_getinfo($sac_curl, CURLINFO_HTTP_CODE);
 		curl_close($sac_curl);
@@ -119,7 +113,7 @@ class SWORDAPPClient {
 			try {
 				// Parse the result
 				$sac_dresponse = new SWORDAPPErrorDocument($sac_status, $sac_resp);
-				
+
 				// Get the deposit results
 				$sac_xml = @new SimpleXMLElement($sac_resp);
                 $sac_ns = $sac_xml->getNamespaces(true);
@@ -156,16 +150,19 @@ class SWORDAPPClient {
 
         unset($sac_atom);
 
-        $xml .= "--===============SWORDPARTS==\n";
-        $xml .= "Content-Type: " . $sac_contenttype . "\n";
-        $xml .= "MIME-Version: 1.0\n";
-        $xml .= "Content-Disposition: attachment; name=\"payload\"; filename=\"package.zip\"\n";
-        $xml .= "Content-Transfer-Encoding: base64\n\n";
-
         $handle = fopen($sac_package, 'rb');
         $file_content = fread($handle, filesize($sac_package));
         fclose($handle);
         $filedata = chunk_split(base64_encode($file_content), 76, "\n");
+
+
+        $xml .= "--===============SWORDPARTS==\n";
+        $xml .= "Content-Type: " . $sac_contenttype . "\n";
+        $xml .= "Content-MD5: " . md5_file($sac_package) . "\n";
+        $xml .= "Content-Length: " . strlen(filedata) . "\n";
+        $xml .= "MIME-Version: 1.0\n";
+        $xml .= "Content-Disposition: attachment; name=\"payload\"; filename=\"package.zip\"\n";
+        $xml .= "Content-Transfer-Encoding: base64\n\n";
 
         $xml .= $filedata;
 
@@ -173,8 +170,7 @@ class SWORDAPPClient {
 
         $xml .= "--===============SWORDPARTS==--\n";
 
-        // To see debugging information, un-comment the following line
-		//curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
+        if ($this->debug) curl_setopt($sac_curl, CURLOPT_VERBOSE, 1);
 
 		curl_setopt($sac_curl, CURLOPT_URL, $sac_url);
 		curl_setopt($sac_curl, CURLOPT_RETURNTRANSFER, true);
@@ -187,13 +183,11 @@ class SWORDAPPClient {
 		$headers = array();
 		global $sal_useragent;
 		array_push($headers, $sal_useragent);
-		$sac_md5 = md5($xml);
-		array_push($headers, "Content-MD5: " . $sac_md5);
 		if (!empty($sac_obo)) {
-			array_push($headers, "X-On-Behalf-Of: " . $sac_obo);
+			array_push($headers, "On-Behalf-Of: " . $sac_obo);
         }
 		if (!empty($sac_packaging)) {
-			array_push($headers, "X-Packaging: " . $sac_packaging);
+			array_push($headers, "Packaging: " . $sac_packaging);
         }
         array_push($headers, "Content-Type: multipart/related; boundary=\"===============SWORDPARTS==\"");
 
