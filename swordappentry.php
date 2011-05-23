236c67f4f1342b3461bfdd8d1a233df5dd327be7
Skylight <stuart@stuartlewis.com>
2011-05-23 20:24:01 +1200

    Fixing indentations and adding code to store the statement IRIs

--- swordappentry.php
+++ swordappentry.php
@@ -5,39 +5,39 @@ require_once("utils.php");
 
 class SWORDAPPEntry {
 
-	// The HTTP status code returned
-	public $sac_status;
-	
-	// The XML returned by the deposit
-	public $sac_xml;
+    // The HTTP status code returned
+    public $sac_status;
 
-	// The human readable status code
-	public $sac_statusmessage;
+    // The XML returned by the deposit
+    public $sac_xml;
 
-	// The atom:id identifier
-	public $sac_id;
+    // The human readable status code
+    public $sac_statusmessage;
 
-	// The atom:content values
-	public $sac_content_src;
-	public $sac_content_type;
-	
-	// The authors
-	public $sac_authors;
+    // The atom:id identifier
+    public $sac_id;
 
-	// The contributors
-	public $sac_contributors;
+    // The atom:content values
+    public $sac_content_src;
+    public $sac_content_type;
 
-	// The links
-	public $sac_links;
+    // The authors
+    public $sac_authors;
 
-	// The title
-	public $sac_title;
+    // The contributors
+    public $sac_contributors;
 
-	// The summary
-	public $sac_summary;
+    // The links
+    public $sac_links;
 
-	// The rights
-	public $sac_rights;
+    // The title
+    public $sac_title;
+
+    // The summary
+    public $sac_summary;
+
+    // The rights
+    public $sac_rights;
 
     // The treatment
     public $sac_treatment;
@@ -45,21 +45,21 @@ class SWORDAPPEntry {
     // The verbose description
     public $sac_verbose_treatment;
 
-	// The update date
-	public $sac_updated;
+    // The update date
+    public $sac_updated;
 
-	// The packaging format used
-	public $sac_packaging;
+    // The packaging format used
+    public $sac_packaging;
 
-	// The generator
-	public $sac_generator;
-	public $sac_generator_uri;
+    // The generator
+    public $sac_generator;
+    public $sac_generator_uri;
 
-	// The user agent
-	public $sac_useragent;
+    // The user agent
+    public $sac_useragent;
 
-	// The noOp status
-	public $sac_noOp;
+    // The noOp status
+    public $sac_noOp;
 
     // Any dcterms metadata
     public $sac_dcterms;
@@ -67,22 +67,28 @@ class SWORDAPPEntry {
     // The Edit IRI
     public $sac_edit_iri;
 
-	// Construct a new deposit response by passing in the http status code
-	function __construct($sac_newstatus, $sac_thexml) {
-		// Store the status
-		$this->sac_status = $sac_newstatus;
+    // The Atom Statement IRI
+    public $sac_state_iri_atom;
 
-		// Store the xml
-		$this->sac_xml = $sac_thexml;
+    // The Atom Statement IRI
+    public $sac_state_iri_ore;
 
-		// Store the status message
-		switch($this->sac_status) {
-			case 201:
-				$this->sac_statusmessage = "Created";
-				break;
-			case 202:
-				$this->sac_statusmessage = "Accepted";
-				break;
+    // Construct a new deposit response by passing in the http status code
+    function __construct($sac_newstatus, $sac_thexml) {
+        // Store the status
+        $this->sac_status = $sac_newstatus;
+
+        // Store the xml
+        $this->sac_xml = $sac_thexml;
+
+        // Store the status message
+        switch($this->sac_status) {
+            case 201:
+                $this->sac_statusmessage = "Created";
+                break;
+            case 202:
+                $this->sac_statusmessage = "Accepted";
+                break;
             case 400:
                 $this->sac_statusmessage = "Bad request";
                 break;
@@ -90,74 +96,83 @@ class SWORDAPPEntry {
                 $this->sac_statusmessage = "Unauthorized";
                 break;
             case 412:
-				$this->sac_statusmessage = "Precondition failed";
-				break;
-			case 413:
-				$this->sac_statusmessage = "Request entity too large";
-				break;
-			case 415:
-				$this->sac_statusmessage = "Unsupported media type";
-				break;
-			default:
-				$this->sac_statusmessage = "Unknown error (status code " . $this->sac_status . ")";
-				break;
-		}
+                $this->sac_statusmessage = "Precondition failed";
+                break;
+            case 413:
+                $this->sac_statusmessage = "Request entity too large";
+                break;
+            case 415:
+                $this->sac_statusmessage = "Unsupported media type";
+                break;
+            default:
+                $this->sac_statusmessage = "Unknown error (status code " . $this->sac_status . ")";
+            break;
+        }
 
-		// Initalise arrays
-		$this->sac_authors = array();
-		$this->sac_contributors = array();
-		$this->sac_links = array();
+        // Initalise arrays
+        $this->sac_authors = array();
+        $this->sac_contributors = array();
+        $this->sac_links = array();
         $this->sac_dcterms = array();
 
-		// Assume noOp is false unless we change it later
-		$this->sac_noOp = false;
-	}
+        // Assume noOp is false unless we change it later
+        $this->sac_noOp = false;
+    }
 
-	// Build the workspace hierarchy
-	function buildhierarchy($sac_dr, $sac_ns) {
-		// Set the default namespace
-		$sac_dr->registerXPathNamespace('atom', 'http://www.w3.org/2005/Atom');
+    // Build the workspace hierarchy
+    function buildhierarchy($sac_dr, $sac_ns) {
+        // Set the default namespace
+        $sac_dr->registerXPathNamespace('atom', 'http://www.w3.org/2005/Atom');
         if (!isset($sac_ns['atom'])) $sac_ns['atom'] = 'http://www.w3.org/2005/Atom';
         if (!isset($sac_ns['dcterms'])) $sac_ns['dcterms'] = 'http://purl.org/dc/terms/';
 
-		// Parse the results
-		$this->sac_id = $sac_dr->children($sac_ns['atom'])->id;
-		$sac_contentbits = $sac_dr->xpath("atom:content"); 
-		if (!empty($sac_contentbits)) {
+        // Parse the results
+        $this->sac_id = $sac_dr->children($sac_ns['atom'])->id;
+        $sac_contentbits = $sac_dr->xpath("atom:content");
+        if (!empty($sac_contentbits)) {
             $this->sac_content_src = $sac_contentbits[0]->attributes()->src;
-			$this->sac_content_type = $sac_contentbits[0]->attributes()->type;
-		}
+            $this->sac_content_type = $sac_contentbits[0]->attributes()->type;
+        }
 
-		// Store the authors
-		foreach ($sac_dr->children($sac_ns['atom'])->author as $sac_author) {
-			$sac_theauthor = $sac_author->children($sac_ns['atom'])->name . "";
-			$this->sac_authors[] = $sac_theauthor;
-		}
-		
-		// Store the contributors
-		foreach ($sac_dr->children($sac_ns['atom'])->contributor as $sac_contributor) {
-			$sac_thecontributor = $sac_contributor->children($sac_ns['atom'])->name . "";
-			$this->sac_contributors[] = $sac_thecontributor;
-		}
-		
-		// Store the links
+        // Store the authors
+        foreach ($sac_dr->children($sac_ns['atom'])->author as $sac_author) {
+            $sac_theauthor = $sac_author->children($sac_ns['atom'])->name . "";
+            $this->sac_authors[] = $sac_theauthor;
+        }
+
+        // Store the contributors
+        foreach ($sac_dr->children($sac_ns['atom'])->contributor as $sac_contributor) {
+            $sac_thecontributor = $sac_contributor->children($sac_ns['atom'])->name . "";
+            $this->sac_contributors[] = $sac_thecontributor;
+        }
+
+        // Store the links
         foreach ($sac_dr->xpath("atom:link") as $sac_link) {
             $sac_linkobject = new SWORDAPPLink($sac_link->attributes()->rel, $sac_link->attributes()->href, $sac_link->attributes()->type);
             array_push($this->sac_links, $sac_linkobject);
 
             // Store the Edit IRI
             if ($sac_linkobject->sac_linkrel == 'edit') $this->sac_edit_iri = $sac_linkobject->sac_linkhref;
-		}
 
-		// Store the title and summary
-		$this->sac_title = sac_clean($sac_dr->children($sac_ns['atom'])->title);
-		$this->sac_summary = sac_clean($sac_dr->children($sac_ns['atom'])->summary);
+            // Store the Statement IRIs
+            if ($sac_linkobject->rel == 'http://purl.org/net/sword/terms/statement') {
+                if ($sac_linkobject->type == 'application/atom+xml;type=feed') {
+                    $this->sac_state_iri_atom = $sac_linkobject->sac_linkhref;
+                } else if ($sac_linkobject->type == 'application/rdf+xml') {
+                    $this->sac_state_iri_atom = $sac_linkobject->sac_linkhref;
+                }
+            }
+        }
 
-		// Store the updated date
-		$this->sac_updated = $sac_dr->children($sac_ns['atom'])->updated;
+        // Store the title and summary
+        $this->sac_title = sac_clean($sac_dr->children($sac_ns['atom'])->title);
+        $this->sac_summary = sac_clean($sac_dr->children($sac_ns['atom'])->summary);
 
-		// Store the rights
-		$this->sac_rights = sac_clean($sac_dr->children($sac_ns['atom'])->rights);
+        // Store the updated date
+        $this->sac_updated = $sac_dr->children($sac_ns['atom'])->updated;
+
+        // Store the rights
+        $this->sac_rights = sac_clean($sac_dr->children($sac_ns['atom'])->rights);
 
         // Store the treatment
         $this->sac_treatment = sac_clean($sac_dr->children($sac_ns['sword'])->treatment);
@@ -165,16 +180,16 @@ class SWORDAPPEntry {
         // Store the verboseDescription
         $this->sac_verbose_treatment = sac_clean($sac_dr->children($sac_ns['sword'])->verboseDescription);
 
-		// Store the format namespace
-		$this->sac_packaging = $sac_dr->children($sac_ns['sword'])->packaging;
+        // Store the format namespace
+        $this->sac_packaging = $sac_dr->children($sac_ns['sword'])->packaging;
 
-		// Store the generator
-		$this->sac_generator = sac_clean($sac_dr->children($sac_ns['atom'])->generator);
-		$sac_gen = $sac_dr->xpath("atom:generator");
-		if (!empty($sac_gen)) { $this->sac_generator_uri = $sac_gen[0]->attributes()->uri; }
+        // Store the generator
+        $this->sac_generator = sac_clean($sac_dr->children($sac_ns['atom'])->generator);
+        $sac_gen = $sac_dr->xpath("atom:generator");
+        if (!empty($sac_gen)) { $this->sac_generator_uri = $sac_gen[0]->attributes()->uri; }
 
-		// Store the user agent
-		$this->sac_useragent = sac_clean($sac_dr->children($sac_ns['sword'])->userAgent);
+        // Store the user agent
+        $this->sac_useragent = sac_clean($sac_dr->children($sac_ns['sword'])->userAgent);
 
         // Store any embedded metadata
         foreach ($sac_dr->children($sac_ns['dcterms']) as $sac_dcterm) {
@@ -184,17 +199,16 @@ class SWORDAPPEntry {
             array_push($this->sac_dcterms[$sac_dcterm->getName()], $sac_dcterm);
         }
 
-		// Store the noOp status
-		if (strtolower((string)$sac_dr->children($sac_ns['sword'])->noOp) == 'true') {
-			$this->sac_noOp = true;
-		}
-	}
+        // Store the noOp status
+        if (strtolower((string)$sac_dr->children($sac_ns['sword'])->noOp) == 'true') {
+            $this->sac_noOp = true;
+        }
+    }
 
     function toString() {
         print " - ID: " . $this->sac_id . "\n";
         print " - Title: " . $this->sac_title . "\n";
-        print " - Content: " . $this->sac_content_src .
-              " (" . $this->sac_content_type . ")\n";
+        print " - Content: " . $this->sac_content_src ." (" . $this->sac_content_type . ")\n";
         foreach ($this->sac_authors as $author) {
             print "  - Author: " . $author . "\n";
         }
@@ -215,8 +229,7 @@ class SWORDAPPEntry {
         print " - Treatment: " . $this->sac_treatment . "\n";
         print " - Verbose description: " . $this->sac_verbose_treatment . "\n";
         print " - Packaging: " . $this->sac_packaging . "\n";
-        print " - Generator: " . $this->sac_generator .
-              " (" . $this->sac_generator_uri . ")\n";
+        print " - Generator: " . $this->sac_generator . " (" . $this->sac_generator_uri . ")\n";
         print " - User agent: " . $this->sac_useragent . "\n";
         if (!empty($this->sac_noOp)) { print " - noOp: " . $this->sac_noOp . "\n"; }
 
