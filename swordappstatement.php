77fc63c27f5a01abeaca2c0a820a885edec7cfd5
Skylight <stuart@stuartlewis.com>
2011-05-25 19:22:10 +1200

    Addition of SWORD Statement (atom serialization)

--- swordappstatement.php
+++ swordappstatement.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once("utils.php");
+require_once('swordappstatemententry.php');
+require_once('utils.php');
 
 class SWORDAPPStatement {
 
@@ -13,10 +14,16 @@ class SWORDAPPStatement {
     // A description of the state of the item
     public $sac_state_description;
 
-    // Construct a new deposit response by passing in the http status code
+    // An array of entries
+    public $sac_entries;
+
+    // Construct a new SWORD statement by passing in the http status code
     function __construct($sac_newstatus, $sac_thexml = '') {
         // Store the xml
         $this->sac_xml = $sac_thexml;
+
+        // Initalise entries
+        $this->sac_entries = array();
        
         // Parse the xml if there is some
 		if ($sac_thexml != '') {
@@ -26,11 +33,35 @@ class SWORDAPPStatement {
             $sac_state_attributes = $sac_state->attributes();
             $this->sac_state_href = $sac_state_attributes['href'];
             $this->sac_state_description = $sac_state->children($sac_ns['sword'])->stateDescription;
+
+            foreach ($sac_statement->children($sac_ns['atom'])->entry as $sac_entry) {
+                $sac_category = $sac_entry->children($sac_ns['atom'])->category;
+                $sac_category_attributes = $sac_category->attributes();
+                $sac_theentry = new SWORDAPPStatementEntry($sac_category_attributes['scheme'],
+                                                           $sac_category_attributes['term'],
+                                                           $sac_category_attributes['label']);
+
+                $sac_content = $sac_entry->children($sac_ns['atom'])->content;
+                $sac_content_attributes = $sac_content->attributes();
+                $sac_theentry->addContent($sac_content_attributes['type'],
+                                          $sac_content_attributes['src']);
+
+                $sac_theentry->setPackaging($sac_entry->children($sac_ns['sword'])->packaging);
+
+                $sac_theentry->setDepositedOn($sac_entry->children($sac_ns['sword'])->depositedOn);
+
+                $sac_theentry->setDepositedBy($sac_entry->children($sac_ns['sword'])->depositedBy);
+
+                array_push($this->sac_entries, $sac_theentry);
+            }
 		}
     }
 
     function toString() {
         print ' - State href: ' . $this->sac_state_href . "\n";
         print ' - State description: ' . $this->sac_state_description . "\n";
+        foreach ($this->sac_entries as $sac_entry) {
+            $sac_entry->toString();
+        }
     }
 }
